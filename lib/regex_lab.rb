3ee9cd6300fd72adbe4490095f66f7b21a0672d3
Ron Sala <ron.sala@gmail.com>
2018-01-08 04:42:02 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,7 +13,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  /^[A-Z]\.*[[:punct:]]/ === text
+  /^[A-Z].*/ === text
 end
 
 def valid_phone_number?(phone)
