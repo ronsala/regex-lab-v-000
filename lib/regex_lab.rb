d242e1e718d87093c9156d9cc34b79fae823421c
Ron Sala <ron.sala@gmail.com>
2018-01-08 04:52:19 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,23 +1,21 @@
 require 'pry'
 
 def starts_with_a_vowel?(word)
-  x = /\b[aeiou]/.match(word)
-  word == 
-  binding.pry
+  /^[AEIOUaeiou].*/ === word
 end
 
-# def words_starting_with_un_and_ending_with_ing(text)
-#   text.scan(/\b/un/\b/ing/)
-# end
+def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/\bun\w*ing\b/)
+end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  /^[A-Z].*\p{P}$/ === text
 end
 
 def valid_phone_number?(phone)
-
+  /\d{3}.*\d{3}.*\d{4}/ === phone
 end
